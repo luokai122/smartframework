0db6e70480afdb61efc99c0686cd94e33bf3b8fc
luokai <1224374053@qq.com>
2018-09-14 08:51:51 +0800

    封装请求信息

---
+++ src/main/java/org/smart4j/framework/bean/Request.java
@@ -0,0 +1,50 @@
+package org.smart4j.framework.bean;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+/**
+ * 封装请求信息
+ * @author StrangeLuo
+ */
+public class Request {
+
+    /**
+     * 请求方法
+     */
+    private String requestMethod;
+
+    /**
+     * 请求路径
+     */
+    private String requestPath;
+
+    /**
+     * 构造方法
+     * @param requestMethod
+     * @param requestPath
+     */
+    public Request(String requestMethod,String requestPath){
+        this.requestMethod = requestMethod;
+        this.requestPath = requestPath;
+    }
+
+    public String getRequestMethod() {
+        return requestMethod;
+    }
+
+    public String getRequestPath() {
+        return requestPath;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return EqualsBuilder.reflectionEquals(this,o);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return HashCodeBuilder.reflectionHashCode(getRequestMethod(), getRequestPath());
+    }
+}
